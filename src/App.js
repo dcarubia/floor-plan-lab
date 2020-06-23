38dbfb1783759431c125edb3b5d4a11a1dc7fd46
Danny Carubia <carubiad1@hawkmail.newpaltz.edu>
2020-06-22 23:24:10 -0700

    detect mobile devices

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
+import { Grid, Typography } from '@material-ui/core';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import AppBar from './layout/AppBar';
 import './App.css';
@@ -8,6 +8,8 @@ import GridContainer from './layout/GridContainer';
 import CoordinateToolTip from './components/CoordinateToolTip';
 import MouseToolTip from './components/MouseToolTip';
 import SetScaleModal from './components/SetScaleModal';
+import { isMobile } from 'react-device-detect';
+import logo from './images/favicon.png';
 
 function App() {
 
@@ -38,24 +40,41 @@ function App() {
   return (
     <div>
       <ThemeProvider theme={theme}>
-        <Grid container>
+        {
+          !isMobile ?
+            <>
+              <Grid container>
 
-          <Grid item xs={12}>
-            <AppBar />
-          </Grid>
+                <Grid item xs={12}>
+                  <AppBar />
+                </Grid>
 
-          <Grid item>
-            <ToolBar />
-          </Grid>
+                <Grid item>
+                  <ToolBar />
+                </Grid>
 
-          <Grid item xs>
-            <GridContainer />
-          </Grid>
-        </Grid>
+                <Grid item xs>
+                  <GridContainer />
+                </Grid>
+              </Grid>
 
-        <CoordinateToolTip />
-        <MouseToolTip />
-        <SetScaleModal />
+              <CoordinateToolTip />
+              <MouseToolTip />
+              <SetScaleModal />
+            </>
+            :
+            <div style={{ position: 'fixed', top: '35%', width: '100vw' }} >
+              <div style={{ display: 'flex', justifyContent: 'center' }}>
+                <img width="112" height="112" src={logo} />
+              </div>
+              <div style={{ display: 'flex', justifyContent: 'center', paddingTop: 16 }}>
+                <Typography variant='h6'>
+                  Coming Soon to mobile platforms!
+              </Typography>
+              </div>
+            </div>
+        }
+
       </ThemeProvider>
     </div>
   );
